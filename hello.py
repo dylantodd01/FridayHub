d9227df8079308600c7c7034f7a326aad4d2811e
Dylan Todd <dylantodd00@gmail.com>
2022-12-13 15:56:45 +0000

    Added pages and image

--- hello.py
+++ hello.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def hello_world():
+@app.route("/home")
+def home_page():
     return render_template('index.html')
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+
+@app.route("/calculator")
+def calculator_page():
+    return render_template('calculator.html')
