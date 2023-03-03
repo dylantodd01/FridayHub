9bc001456aafeac59a6e10a9fc24dcb1e117108d
lbg-isabel-frolick <isabel.frolick@lloydsbanking.com>
2023-03-03 14:38:14 +0000

    Dockerisation

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 import yfinance as yf
 import streamlit as st
-from datetime import date
+from datetime import date 
 from prophet import Prophet
 from prophet.plot import plot_plotly
+from plotly import graph_objs as go
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
